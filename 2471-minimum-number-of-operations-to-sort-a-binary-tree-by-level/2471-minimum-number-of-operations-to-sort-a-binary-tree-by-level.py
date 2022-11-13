7c50880f212c83cb1ce5630402fbafd0540f5705
Qasim <89395826+kaszmm@users.noreply.github.com>
2022-11-13 23:45:34 +0530

    Time: 828 ms (77.78%), Space: 54.6 MB (11.11%) - LeetHub

---
+++ 2471-minimum-number-of-operations-to-sort-a-binary-tree-by-level/2471-minimum-number-of-operations-to-sort-a-binary-tree-by-level.py
@@ -0,0 +1,55 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    
+    def minSwapNeeded(self,temp):
+        i=0
+        count=0
+        hashMap=defaultdict(int)
+        sortedTemp=[]
+        for i in range(len(temp)):
+            hashMap[temp[i]]=i
+            sortedTemp.append(temp[i])
+        sortedTemp.sort()
+        for i in range(len(temp)):
+            if temp[i]!=sortedTemp[i]:
+                p = hashMap[sortedTemp[i]]
+                hashMap[sortedTemp[i]] = i
+                hashMap[temp[i]] = p
+                temp[i], temp[p]=temp[p], temp[i]
+                count+=1            
+        return count  
+            
+            
+            
+            
+    def minimumOperations(self, root: Optional[TreeNode]) -> int:
+        queue=deque()
+        queue.append(root)
+        count=0
+        while queue:
+            qLength=len(queue)
+            temp=[]
+            for i in range(0,qLength):
+                node=queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                    temp.append(node.left.val)
+                if node.right:
+                    queue.append(node.right)
+                    temp.append(node.right.val)
+              
+            count+=self.minSwapNeeded(temp)
+        return count
+            
+                
+                
+                
+                    
+                
+            
+                    
