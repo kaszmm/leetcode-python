bed9465ab816948034dbf4756936216021e7a50a
Qasim <89395826+kaszmm@users.noreply.github.com>
2022-11-15 10:07:26 +0530

    Time: 654 ms (97.93%), Space: 30.4 MB (46.18%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,29 +1,24 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
+        dp = [[False for j in range(len(s))] for i in range(len(s))]
         res=[]
-        partitions=[]
-        def isPalindrome(s,i,j):
-            while i<j:
-                if s[i]!=s[j]:
-                    return False
-                i+=1
-                j-=1
-            return True
-                 
-                
-        def dfs(idx):
-            if idx>=len(s):
-                res.append(partitions[:])
+        for i in range(len(s)):
+            for j in range(0,i+1):
+                if s[i]==s[j] and (i-j<3 or dp[i-1][j+1]):
+                    dp[i][j]=True
+        
+        
+        def dfs(curIdx,palindromes):
+            if curIdx>=len(s):
+                res.append(palindromes[:])
                 return
-            for i in range(idx,len(s)):
-                if isPalindrome(s,idx,i):
-                    partitions.append(s[idx:i+1])
-                    dfs(i+1)
-                    partitions.pop()
+            for i in range(curIdx,len(s)):
+                if dp[i][curIdx]:
+                    palindromes.append(s[curIdx:i+1])
+                    dfs(i+1,palindromes)
+                    palindromes.pop()
             return
-        
-        dfs(0)
+        dfs(0,[])
         return res
-        
                     
             
