4cf5e2374eb1404b6ad8ab9a8b0ac4780bd52fac
Qasim <89395826+kaszmm@users.noreply.github.com>
2022-11-16 08:15:25 +0530

    Time: 65 ms (21.34%), Space: 13.8 MB (80.34%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -2,28 +2,23 @@ class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         if not len(digits):
             return []
-        numberDict=defaultdict(list)
-        numberDict['2']=['a','b','c']
-        numberDict['3']=['d','e','f']
-        numberDict['4']=['g','h','i']
-        numberDict['5']=['j','k','l']
-        numberDict['6']=['m','n','o']
-        numberDict['7']=['p','q','r','s']
-        numberDict['8']=['t','u','v']
-        numberDict['9']=['w','x','y','z']
-        
-        res=[]
-        def dfs(curStr,idx):
-            if idx>=len(digits):
-                res.append(curStr)
-                return
-            aplhas=numberDict[digits[idx]]
-            for i in range(0,len(aplhas)):
-                curStr+=aplhas[i]
-                dfs(curStr,idx+1)
-                curStr=curStr[:-1]
-                
-            return
-        
-        dfs("",0)
+        res=deque()
+        res.append("")
+        digitToChar = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "qprs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
+        for digit in digits:
+            resLength=len(res)
+            for i in range(resLength):
+                removedChar=res.popleft()
+                for character in digitToChar[digit]:
+                    res.append(removedChar+character)
+            
         return res
