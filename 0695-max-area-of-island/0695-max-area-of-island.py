432dcbdfc5f56ae84d5fe4953a1a33781f283645
Qasim <89395826+kaszmm@users.noreply.github.com>
2022-11-17 22:54:03 +0530

    Time: 365 ms (35.03%), Space: 16.7 MB (45.39%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,29 +1,24 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        self.maxArea=0
-        self.area=0
+        maxArea=0
         ROWS=len(grid)
         COLUMNS=len(grid[0])
-        def calculateArea(r,c):
+        def calculateArea(r,c,area):
             if r<0 or r>=ROWS or c<0 or c>=COLUMNS or grid[r][c]==0:
-                return
-            
+                return 0
             grid[r][c]=0
-            self.area+=1
-            self.maxArea=max(self.maxArea,self.area)
-            calculateArea(r-1,c)
-            calculateArea(r+1,c)
-            calculateArea(r,c-1)
-            calculateArea(r,c+1)
-            return
-        
-        
+            area+=1
+            area=max(calculateArea(r-1,c,area),area)  #down
+            area=max(calculateArea(r+1,c,area),area)  #up
+            area=max(calculateArea(r,c-1,area),area)  #left
+            area=max(calculateArea(r,c+1,area),area)  #right
+            return area
         for row in range(ROWS):
             for col in range(COLUMNS):
                 if grid[row][col]>0:
-                    self.area=0
-                    calculateArea(row,col)
-        return self.maxArea
+                    area=calculateArea(row,col,0)
+                    maxArea=max(maxArea,area)
+        return maxArea
                 
         
         
