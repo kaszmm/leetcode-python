374ddfa4a629067dc207ec8454cbd6fc2a5043b5
Qasim <89395826+kaszmm@users.noreply.github.com>
2022-11-17 09:15:14 +0530

    Time: 99 ms (77.00%), Space: 14.5 MB (13.05%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -2,27 +2,47 @@ class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
         res=[]
         board=[["."]*n for i in range(n)]
-        avoidCols=set()
-        avoidDownwardDiag=set()
-        avoidUpwardDiag=set()
+    
+        def IsQueenPresent(row,col):
+            tempRow=row
+            tempCol=col
+            #check if Queen present in same column
+            while tempRow>=0:
+                if board[tempRow][tempCol]=="Q":
+                    return True
+                tempRow-=1
+            
+            tempRow=row
+            tempCol=col
+            #check if Queen present in leftside upward diagonal
+            while tempRow>=0 and tempCol>=0:
+                if board[tempRow][tempCol]=="Q":
+                    return True
+                tempRow-=1
+                tempCol-=1
+            
+            tempRow=row
+            tempCol=col
+            #check if Queen present in rightside upward diagonal
+            while tempRow>=0 and tempCol<n:
+                if board[tempRow][tempCol]=="Q":
+                    return True
+                tempRow-=1
+                tempCol+=1
+            return False
+    
         def dfs(curRow):
-            if curRow>=n:
-                dummyBoard=["".join(r) for r in board]
+            if curRow==n:
+                dummyBoard=["".join(row) for row in board]
                 res.append(dummyBoard[:])
                 return
             for col in range(n):
-                if col not in avoidCols and col-curRow not in avoidDownwardDiag and col+curRow not in avoidUpwardDiag:
-                    avoidCols.add(col)
-                    avoidDownwardDiag.add(col-curRow)
-                    avoidUpwardDiag.add(col+curRow)
+                if not IsQueenPresent(curRow,col):
                     board[curRow][col]="Q"
                     dfs(curRow+1)
-                    avoidCols.remove(col)
-                    avoidDownwardDiag.remove(col-curRow)
-                    avoidUpwardDiag.remove(col+curRow)
                     board[curRow][col]="."
+                    
         dfs(0)
         return res
-                
             
             
