49255373857533770024ff44a5d0f071c38b88f3
Qasim <89395826+kaszmm@users.noreply.github.com>
2022-11-17 23:11:54 +0530

    Time: 302 ms (57.99%), Space: 17.8 MB (18.44%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -3,20 +3,16 @@ class Solution:
         maxArea=0
         ROWS=len(grid)
         COLUMNS=len(grid[0])
-        def calculateArea(r,c,area):
-            if r<0 or r>=ROWS or c<0 or c>=COLUMNS or grid[r][c]==0:
+        visited=set()
+        def calculateArea(r,c):
+            if r<0 or r>=ROWS or c<0 or c>=COLUMNS or grid[r][c]==0 or (r,c) in visited:
                 return 0
-            grid[r][c]=0
-            area+=1
-            area=max(calculateArea(r-1,c,area),area)  #down
-            area=max(calculateArea(r+1,c,area),area)  #up
-            area=max(calculateArea(r,c-1,area),area)  #left
-            area=max(calculateArea(r,c+1,area),area)  #right
-            return area
+            visited.add((r,c))
+            return 1 + calculateArea(r-1,c) + calculateArea(r+1,c) +calculateArea(r,c-1) + calculateArea(r,c+1)
         for row in range(ROWS):
             for col in range(COLUMNS):
-                if grid[row][col]>0:
-                    area=calculateArea(row,col,0)
+                if grid[row][col]>0 and (row,col) not in visited:
+                    area=calculateArea(row,col)
                     maxArea=max(maxArea,area)
         return maxArea
                 
