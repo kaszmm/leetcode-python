81dc0131991ca7ccfa9e65b4339a741019db04a6
Qasim <89395826+kaszmm@users.noreply.github.com>
2022-11-18 23:54:11 +0530

    Time: 660 ms (7.05%), Space: 59.1 MB (7.58%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -0,0 +1,54 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        ROWS=len(board)
+        COLUMNS=len(board[0])
+        visitedCircles=set()
+        
+        def getDeadCircles(row,col):
+            if row<0 or row>=ROWS or col<0 or col>=COLUMNS or board[row][col]=="X" or (row,col) in visitedCircles:
+                return
+            visitedCircles.add((row,col))
+            getDeadCircles(row-1,col)
+            getDeadCircles(row+1,col)
+            getDeadCircles(row,col-1)
+            getDeadCircles(row,col+1)
+            return
+        
+        for col in range(COLUMNS):
+            if  board[0][col]=="O" and (0,col) not in visitedCircles:
+                getDeadCircles(0,col)
+            if  board[ROWS-1][col]=="O" and (ROWS-1,col) not in visitedCircles:
+                getDeadCircles(ROWS-1,col)
+        
+        for row in range(ROWS):
+            if board[row][0]=="O" and (row,0) not in visitedCircles:
+                getDeadCircles(row,0)
+            if board[row][COLUMNS-1]=="O" and (row,COLUMNS-1) not in visitedCircles:
+                getDeadCircles(row,COLUMNS-1)
+                
+        def flipCircle(row,col):
+            if row<0 or row>=ROWS or col<0 or col>=COLUMNS or board[row][col]=="X" or (row,col) in visitedCircles:
+                return
+            board[row][col]="X"
+            visitedCircles.add((row,col))
+            flipCircle(row-1,col)
+            flipCircle(row+1,col)
+            flipCircle(row,col-1)
+            flipCircle(row,col+1)
+            return
+        
+        for row in range(1,ROWS-1):
+            for col in range(1,COLUMNS-1):
+                if board[row][col]=="O" and (row,col) not in visitedCircles:
+                    flipCircle(row,col)
+        
+        
+        
+        
+        
+        
+        
+        
