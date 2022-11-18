b2e1449aaf977cf6d543dee69e9bdea889dd5935
Qasim <89395826+kaszmm@users.noreply.github.com>
2022-11-19 00:13:48 +0530

    Time: 311 ms (58.59%), Space: 15.3 MB (85.80%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -5,12 +5,11 @@ class Solution:
         """
         ROWS=len(board)
         COLUMNS=len(board[0])
-        visitedCircles=set()
         
         def getDeadCircles(row,col):
-            if row<0 or row>=ROWS or col<0 or col>=COLUMNS or board[row][col]=="X" or (row,col) in visitedCircles:
+            if row<0 or row>=ROWS or col<0 or col>=COLUMNS or board[row][col]!="O":
                 return
-            visitedCircles.add((row,col))
+            board[row][col]="-"
             getDeadCircles(row-1,col)
             getDeadCircles(row+1,col)
             getDeadCircles(row,col-1)
@@ -18,22 +17,27 @@ class Solution:
             return
         
         for col in range(COLUMNS):
-            if  board[0][col]=="O" and (0,col) not in visitedCircles:
+            if  board[0][col]=="O":
                 getDeadCircles(0,col)
-            if  board[ROWS-1][col]=="O" and (ROWS-1,col) not in visitedCircles:
+            if  board[ROWS-1][col]=="O":
                 getDeadCircles(ROWS-1,col)
         
         for row in range(ROWS):
-            if board[row][0]=="O" and (row,0) not in visitedCircles:
+            if board[row][0]=="O":
                 getDeadCircles(row,0)
-            if board[row][COLUMNS-1]=="O" and (row,COLUMNS-1) not in visitedCircles:
+            if board[row][COLUMNS-1]=="O":
                 getDeadCircles(row,COLUMNS-1)
                 
         
         for row in range(1,ROWS-1):
             for col in range(1,COLUMNS-1):
-                if board[row][col]=="O" and (row,col) not in visitedCircles:
+                if board[row][col]=="O":
                     board[row][col]="X"
+        
+        for row in range(ROWS):
+            for col in range(COLUMNS):
+                if board[row][col]=="-":
+                    board[row][col]="O"
         
         
         
