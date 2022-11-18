3cdb51efa0b5b5f00fd6e0a8707fd904a31405ea
Qasim <89395826+kaszmm@users.noreply.github.com>
2022-11-18 09:04:59 +0530

    Time: 268 ms (67.30%), Space: 14.8 MB (73.91%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -3,17 +3,45 @@ class Solution:
         maxArea=0
         ROWS=len(grid)
         COLUMNS=len(grid[0])
-        visited=set()
-        def calculateArea(r,c):
-            if r<0 or r>=ROWS or c<0 or c>=COLUMNS or grid[r][c]==0 or (r,c) in visited:
-                return 0
-            visited.add((r,c))
-            return 1 + calculateArea(r-1,c) + calculateArea(r+1,c) +calculateArea(r,c-1) + calculateArea(r,c+1)
+    
+        parents=defaultdict()
+        rank=defaultdict()
+        directions=[(1,0),(-1,0),(0,1),(0,-1)]
         for row in range(ROWS):
             for col in range(COLUMNS):
-                if grid[row][col]>0 and (row,col) not in visited:
-                    area=calculateArea(row,col)
-                    maxArea=max(maxArea,area)
+                if grid[row][col]>0:
+                    parents[(row,col)]=(row,col)
+                    rank[(row,col)]=1
+    
+        def find(r,c):
+            parent=parents[(r,c)]
+            while parent!=parents[parent]:
+                parent=parents[parents[parent]]
+            return parent
+        
+        def union(r1,c1,r2,c2):
+            parent1=find(r1,c1)
+            parent2=find(r2,c2)
+            if parent1==parent2:
+                return rank[parent1]
+            if rank[parent1]>rank[parent2]:
+                rank[parent1]+=rank[parent2]
+                parents[parent2]=parent1
+                return rank[parent1]
+            else:
+                rank[parent2]+=rank[parent1]
+                parents[parent1]=parent2
+                return rank[parent2]
+    
+        for row in range(ROWS):
+            for col in range(COLUMNS):
+                if grid[row][col]>0:
+                    area=0
+                    maxArea=max(maxArea,1)
+                    for r,c in directions:
+                        if row+r>=0 and row+r<ROWS and col+c>=0 and col+c<COLUMNS and grid[row+r][col+c]>0:
+                            area=union(row,col,row+r,col+c)
+                            maxArea=max(maxArea,area if area>0 else 1)
         return maxArea
                 
         
