7c21fd6ead44ecaaacaa9e6e67b38765830ce68e
Qasim <89395826+kaszmm@users.noreply.github.com>
2022-11-20 00:00:40 +0530

    Time: 834 ms (5.01%), Space: 20.7 MB (15.66%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -5,40 +5,57 @@ class Solution:
         """
         ROWS=len(board)
         COLUMNS=len(board[0])
-        rowBounds=[0,ROWS-1]
-        colBounds=[0,COLUMNS-1]
-        queue=deque()
-
-        def inBound(r,c):
-            if r>=0 and r<ROWS and c>=0 and c<COLUMNS:
-                return True
-            else:
-                return False
-        def bfs():
-            while queue:
-                r,c=queue.popleft()
-                if inBound(r,c) and board[r][c]=="O":
-                    board[r][c]="-"
-                    queue.append((r+1,c))
-                    queue.append((r-1,c))
-                    queue.append((r,c+1))
-                    queue.append((r,c-1))
-            return
+        parents={i:i for i in range(ROWS*COLUMNS+1)}
+        rank={i:1 for i in range(ROWS*COLUMNS+1)}
+        directions=[(1,0),(-1,0),(0,1),(0,-1)]
         
-        for col in range(COLUMNS):
-            queue.append((0,col))
-            queue.append((ROWS-1,col))
+        def find(parent):
+            parent=parents[parent]
+            while parent!=parents[parent]:
+                parents[parent]=parents[parents[parent]]
+                parent=parents[parent]
+            return parent
+        
+        def union(parent1,parent2):
+            p1=find(parent1)
+            p2=find(parent2)
+            if p1==p2:
+                return
+            if rank[p1]>rank[p2]:
+                rank[p1]+=rank[p2]
+                parents[p2]=p1
+            else:
+                rank[p2]+=rank[p1]
+                parents[p1]=p2
+            return
                     
-        for row in range(ROWS):
-            queue.append((row,0))
-            queue.append((row,COLUMNS-1))
-        bfs()    
+        def isConnected(p1,p2):
+            return find(p1)==find(p2)
+        def getPosition(r,c):
+            return r*COLUMNS+c
+        
         for row in range(ROWS):
             for col in range(COLUMNS):
                 if board[row][col]=="O":
-                    board[row][col]="X"
-                elif board[row][col]=="-":
-                    board[row][col]="O"
+                    if row==0 or col==0 or row==ROWS-1 or col==COLUMNS-1:
+                        union(getPosition(row,col),ROWS*COLUMNS)
+                    else:
+                        for d1,d2 in directions:
+                            if board[row+d1][col+d2]=="O":
+                                union(getPosition(row,col),getPosition(row+d1,col+d2))
+        for row in range(ROWS):
+            for col in range(COLUMNS):
+                if board[row][col]=="O":
+                    if not isConnected(getPosition(row,col),ROWS*COLUMNS):
+                        board[row][col]="X"
+                            
+                        
+                    
+                    
+                    
+                
+            
+            
                     
                     
                 
