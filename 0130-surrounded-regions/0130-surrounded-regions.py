ed1d13eccc1a5a9dd003b291ce4f7664e48f4a73
Qasim <89395826+kaszmm@users.noreply.github.com>
2022-11-19 09:34:23 +0530

    Time: 171 ms (83.58%), Space: 15.5 MB (83.62%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -5,36 +5,43 @@ class Solution:
         """
         ROWS=len(board)
         COLUMNS=len(board[0])
-        
-        def getDeadCircles(row,col):
-            if row<0 or row>=ROWS or col<0 or col>=COLUMNS or board[row][col]!="O":
-                return
-            board[row][col]="-"
-            getDeadCircles(row-1,col)
-            getDeadCircles(row+1,col)
-            getDeadCircles(row,col-1)
-            getDeadCircles(row,col+1)
+        rowBounds=[0,ROWS-1]
+        colBounds=[0,COLUMNS-1]
+        queue=deque()
+
+        def inBound(r,c):
+            if r>=0 and r<ROWS and c>=0 and c<COLUMNS:
+                return True
+            else:
+                return False
+        def bfs():
+            while queue:
+                r,c=queue.popleft()
+                if inBound(r,c) and board[r][c]=="O":
+                    board[r][c]="-"
+                    queue.append((r+1,c))
+                    queue.append((r-1,c))
+                    queue.append((r,c+1))
+                    queue.append((r,c-1))
             return
         
         for col in range(COLUMNS):
-            if  board[0][col]=="O":
-                getDeadCircles(0,col)
-            if  board[ROWS-1][col]=="O":
-                getDeadCircles(ROWS-1,col)
-        
+            queue.append((0,col))
+            queue.append((ROWS-1,col))
+                    
         for row in range(ROWS):
-            if board[row][0]=="O":
-                getDeadCircles(row,0)
-            if board[row][COLUMNS-1]=="O":
-                getDeadCircles(row,COLUMNS-1)
-                
-        
+            queue.append((row,0))
+            queue.append((row,COLUMNS-1))
+        bfs()    
         for row in range(ROWS):
             for col in range(COLUMNS):
                 if board[row][col]=="O":
                     board[row][col]="X"
                 elif board[row][col]=="-":
                     board[row][col]="O"
+                    
+                    
+                
         
         
         
