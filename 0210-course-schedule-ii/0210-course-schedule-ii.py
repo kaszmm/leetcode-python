2192e9d96a3f1fcad7ae6feca8f970390f4548c7
Qasim <89395826+kaszmm@users.noreply.github.com>
2022-11-23 11:17:06 +0530

    Time: 7201 ms (5.01%), Space: 20 MB (5.15%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -0,0 +1,28 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        adj={i :set() for i in range(numCourses)}
+        for course,preq in prerequisites:
+            adj[course].add(preq)
+        
+        doneCourses=set()
+        res=[]
+        
+        def dfs(visited,course):
+            if len(adj[course])==0:
+                return True
+            visited.add(course)
+            for preq in adj[course].copy():
+                if preq in doneCourses:
+                    adj[course].remove(preq)
+                else:
+                    if preq not in visited and dfs(visited,preq):
+                        doneCourses.add(preq)
+                        res.append(preq)
+                        adj[course].remove(preq)
+            return len(adj[course])==0
+        for i in range(numCourses):
+            visited=set()
+            if i not in doneCourses and dfs(visited,i):
+                doneCourses.add(i)
+                res.append(i)
+        return res if len(doneCourses)==numCourses else []
