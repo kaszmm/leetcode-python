a43b88b98451e32249374b99776d3c0164a0f786
Qasim <89395826+kaszmm@users.noreply.github.com>
2022-11-24 22:44:01 +0530

    Time: 199 ms (6.01%), Space: 14.6 MB (28.09%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -1,18 +1,27 @@
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
         adj=defaultdict(list)
-        def detectCycle(visited:set,prev,node):
-            if node in visited:
-                return True
+        def detectCycle(node):
+            queue=deque()
+            visited=set()
+            prevVisited=set()
+            queue.append(node)
             visited.add(node)
-            for neighbours in adj[node]:
-                if neighbours!=prev and detectCycle(visited,node,neighbours):
-                    return True
+            while queue:
+                for i in range(len(queue)):
+                    curNode=queue.popleft()
+                    prevVisited.add(curNode)
+                    for neighbors in adj[curNode]:
+                        if neighbors not in prevVisited:
+                            if neighbors in visited:
+                                return True
+                            queue.append(neighbors)
+                            visited.add(neighbors)
             return False
 
         for edge in edges:
             adj[edge[0]].append(edge[1])
             adj[edge[1]].append(edge[0])
-            if detectCycle(set(),-1,edge[0]):
+            if detectCycle(edge[0]):
                 return edge
         
