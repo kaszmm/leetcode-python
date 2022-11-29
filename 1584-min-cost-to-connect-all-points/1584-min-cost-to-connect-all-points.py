7b464636fae6e68c86379eef3584c4ad53d34073
Qasim <89395826+kaszmm@users.noreply.github.com>
2022-11-30 00:30:25 +0530

    Time: 7380 ms (14.03%), Space: 174.9 MB (16.79%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -1,30 +1,28 @@
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        adj=defaultdict(list)
-        visited=set()
-        frontier=[[0,0]]
-        for i in range(len(points)):
-            for j in range(i+1,len(points)):
-                x1,y1=points[i]
-                x2,y2=points[j]
-                dist=abs(x1-x2)+abs(y1-y2)
-                adj[i].append([dist,j])
-                adj[j].append([dist,i])
-                
-                
-                
-        res=0
-        while len(visited)!=len(points):
-            dist,point=heapq.heappop(frontier)
-            if point in visited:
+        N = len(points)
+        adj = {i: [] for i in range(N)}  # i : list of [cost, node]
+        for i in range(N):
+            x1, y1 = points[i]
+            for j in range(i + 1, N):
+                x2, y2 = points[j]
+                dist = abs(x1 - x2) + abs(y1 - y2)
+                adj[i].append([dist, j])
+                adj[j].append([dist, i])
+
+        # Prim's
+        res = 0
+        visit = set()
+        minH = [[0, 0]]  # [cost, point]
+        while len(visit) < N:
+            cost, i = heapq.heappop(minH)
+            if i in visit:
                 continue
-            visited.add(point)
-            res+=dist
-            for cost,p in adj[point]:
-                if p in visited: continue
-                heapq.heappush(frontier,[cost,p])
+            res += cost
+            visit.add(i)
+            for neiCost, nei in adj[i]:
+                if nei not in visit:
+                    heapq.heappush(minH, [neiCost, nei])
         return res
-                
-        
         
             
