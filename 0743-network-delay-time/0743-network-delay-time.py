bbf60afa7776400bb2efaa3f5c1d23db5507c8d3
Qasim <89395826+kaszmm@users.noreply.github.com>
2022-12-02 23:34:41 +0530

    Time: 5181 ms (5.01%), Space: 16 MB (70.05%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -1,18 +1,25 @@
 class Solution:
-    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        distances=[float("INF")]*(n+1)
-        result=0
-        distances[0],distances[k]=0,0
+    def networkDelayTime(self, times: List[List[int]], n: int, s: int) -> int:
+        dist=[[float("inf")]*(n) for i in range(n)]
+        res=0
+        for i in range(n):
+            dist[i][i]=0
         
-        for i in range(1,n):
-            for trans,receive,weight in times:
-                distances[receive]=min(distances[receive],distances[trans]+weight)
+        for t,r,w in times:
+            dist[t-1][r-1]=w
         
-        for cost in distances:
-            if cost==float("inf"):
+        for i in range(n):
+            for j in range(n):
+                for k in range(n):
+                    if j==i or k==i:
+                        continue
+                    dist[j][k]=min(dist[j][k],dist[j][i]+dist[i][k])
+        
+        for j in range(n):
+            if dist[s-1][j]==float("inf"):
                 return -1
-            result=max(result,cost)
-        return result
+            res=max(res,dist[s-1][j])
+        return res if res!=float("inf") else -1
                 
             
             
