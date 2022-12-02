5d0381894e49e1b94d8b3191e47dc473ff70b7f1
Qasim <89395826+kaszmm@users.noreply.github.com>
2022-12-03 00:28:21 +0530

    Time: 561 ms (80.51%), Space: 16.8 MB (11.21%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -1,24 +1,21 @@
 class Solution:
-    def networkDelayTime(self, times: List[List[int]], n: int, s: int) -> int:
-        dist=[[float("inf")]*(n) for i in range(n)]
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        dist=[float("inf")]*(n+1)
+        dist[0]=0
+        queue=deque()
+        queue.append([k,0])
         res=0
-        for i in range(n):
-            dist[i][i]=0
-        
+        adj=defaultdict(list)
         for t,r,w in times:
-            dist[t-1][r-1]=w
-        
-        for i in range(n):
-            for j in range(n):
-                for k in range(n):
-                    if j==i or k==i:
-                        continue
-                    dist[j][k]=min(dist[j][k],dist[j][i]+dist[i][k])
-        
-        for j in range(n):
-            if dist[s-1][j]==float("inf"):
-                return -1
-            res=max(res,dist[s-1][j])
+            adj[t].append([r,w])
+    
+        while queue:
+            t,w=queue.popleft()
+            if w<dist[t]:
+                dist[t]=w
+                for r,d in adj[t]:
+                    queue.append([r,d+w])
+        res=max(dist)
         return res if res!=float("inf") else -1
                 
             
