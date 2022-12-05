39fb7e32b8e36993cda12a29b70119884befa614
Qasim <89395826+kaszmm@users.noreply.github.com>
2022-12-06 01:03:21 +0530

    Time: 1822 ms (5.01%), Space: 16.1 MB (6.18%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.py
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.py
@@ -1,29 +1,44 @@
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
         visited=set()
-        ROWS=len(grid)
-        COLUMNS=len(grid[0])
+        n=len(grid)
         directions=[(1,0),(-1,0),(0,1),(0,-1)]
         def inBound(r,c):
-            if r<0 or r>=ROWS or c<0 or c>=COLUMNS:
+            if r<0 or r>=n or c<0 or c>=n:
                 return False
             return True
-        res=float("inf")
-        minHeap=[[grid[0][0],0,0]]
-        while minHeap:
-            cost,r,c =heapq.heappop(minHeap)
-            if r==ROWS-1 and c==COLUMNS-1:
-                res=min(res,cost)
-                break
-            if (r,c) in visited:
-                continue
+        def getPosition(r,c):
+            return r*n+c
+        parents=[0]*(n*n)
+        positions=[]
+        for i in range(n):
+            for j in range(n):
+                pos=getPosition(i,j)
+                parents[pos]=pos
+                positions.append([grid[i][j],i,j])
+            
+        def find(p):
+            if p==parents[p]:
+                return p
+            return find(parents[p])
+        
+        def union(x1,x2):
+            p1=find(x1)
+            p2=find(x2)
+            
+            if p1==p2:
+                return False
+            parents[p2]=p1
+            return True
+        positions.sort(key=lambda x:x[0])
+        for  cost,r,c in positions:
             visited.add((r,c))
-    
-            for rd,cd in directions:
-                r1=r+rd
-                c1=c+cd
-                if inBound(r1,c1) and (r1,c1) not in visited:
-                    heapq.heappush(minHeap,[max(cost,grid[r1][c1]),r1,c1])
-        return res
+            for dr,dc in directions:
+                if inBound(r+dr,c+dc) and (r+dr,c+dc) in visited:
+                    union(getPosition(r,c),getPosition(r+dr,c+dc))
+            if find(getPosition(0,0))==find(getPosition(n-1,n-1)):
+                return cost
+                        
+        return -1
             
             
